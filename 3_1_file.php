e8f083fc9a2be2e57c175ce88bdacfd9a4a79f24
filipkuczewski <filipkuczewski@gmail.com>
2020-03-08 15:05:32 +0100

    Wszystko z zajec

---
+++ 3_1_file.php
@@ -0,0 +1,9 @@
+<?php
+
+echo <<<X
+<hr>
+
+Plik o nazwie: 3_1_file.php
+<hr>
+X;
+?>
---
+++ 3_dolaczanie_plikow.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Plik główny</h3>
+    <?php
+
+include_once './3_1_file.php';
+include_once './3_1_file.php';
+
+require_once './3_1_file.php';
+?>
+<h3>Koniec Pliku Głownego</h3>
+ 
+</body>
+</html>
